f70ab108047aba0e1c91b422c5503fa19ea9a6be
MerkulovDaniil <10253272+MerkulovDaniil@users.noreply.github.com>
2021-03-01 17:49:26 +0300

    Update draw_net.py

--- neural_architecture/draw_net.py
+++ neural_architecture/draw_net.py
@@ -33,14 +33,18 @@ def draw_net(model, input_size = (1, 32, 32), figure_name = None):
         if layer_type == 'Conv2d':
             nn.add(Conv2D())
         elif layer_type == 'Linear':
-            nn.add(Dense())
+            # print(layer_dict)
+            num_un = int(5 + 10/(1 + np.exp(-0.1*layer_dict['output_shape'][1])))
+            nn.add(DenseLayer(num_units = num_un))
         elif layer_type == 'MaxPool2d':
             pool_size = int(round(layer_dict['input_shape'][-1]/layer_dict['output_shape'][-1]))
             nn.add(Pooling(pool_size=pool_size))
+        elif layer_type == 'BatchNorm2d':
+            nn.add(BatchNorm())
     
     # print(nn.layers)
     nn.compile()
-    nn.add_legend(bbox_to_anchor=(0.2, -0.1, 0.6, 0.1), fontsize=26)
+    nn.add_legend(bbox_to_anchor=(0.2, -0.1, 0.6, 0.1), fontsize=20)
     if figure_name is not None:
         nn.draw(save=True, save_path=figure_name)
     else:
