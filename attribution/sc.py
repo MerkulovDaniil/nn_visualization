e0c243b1bacf07083ee0bba72f8c5520d2d63b59
AndreiChertkov <a.chertkov@skoltech.ru>
2021-12-24 18:50:29 +0300

    Update code for Score-CAM

--- attribution/sc.py
+++ attribution/sc.py
@@ -2,39 +2,27 @@ import torch
 import torch.nn.functional as F
 
 
-class ScoreCAM(object):
-
+class ScoreCAM:
     def __init__(self, model, target_layer, device):
         self.model = model
         self.target_layer = target_layer
         self.device = device
 
-        self.gradients = dict()
-        self.activations = dict()
+        self.gradients = {}
+        self.activations = {}
 
         def backward_hook(module, grad_input, grad_output):
-            if self.device.type == 'cuda':
-              self.gradients['value'] = grad_output[0].cuda()
-            else:
-              self.gradients['value'] = grad_output[0]
-            return None
+            self.gradients['value'] = grad_output[0].to(self.device)
 
         def forward_hook(module, input, output):
-            if self.device.type == 'cuda':
-              self.activations['value'] = output.cuda()
-            else:
-              self.activations['value'] = output
-            return None
+            self.activations['value'] = output.to(self.device)
 
         self.target_layer.register_forward_hook(forward_hook)
         self.target_layer.register_backward_hook(backward_hook)
 
-
     def forward(self, input, class_idx=None, retain_graph=False):
         b, c, h, w = input.size()
         logit = self.model(input)
-        if self.device.type == 'cuda':
-            logit.cuda()
 
         if class_idx is None:
             predicted_class = logit.max(1)[-1]
@@ -50,43 +38,37 @@ class ScoreCAM(object):
         score_saliency_map = torch.zeros((1, 1, h, w), device=self.device)
         activations = self.activations['value']
         b, k, u, v = activations.size()
-        if self.device.type == 'cuda':
-          activations.cuda()
+        activations = activations.to(self.device)
 
         with torch.no_grad():
           for i in range(k):
-
-              # upsampling
               saliency_map = torch.unsqueeze(activations[:, i, :, :], 1)
-              saliency_map = F.interpolate(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
-              if self.device.type == 'cuda':
-                saliency_map.cuda()
+              saliency_map = F.interpolate(
+                saliency_map, size=(h, w), mode='bilinear', align_corners=False)
+              saliency_map = saliency_map.to(self.device)
               if saliency_map.max() == saliency_map.min():
                 continue
 
-              # normalize to 0-1
-              norm_saliency_map = (saliency_map - saliency_map.min()) / (saliency_map.max() - saliency_map.min())
+              norm_saliency_map = (saliency_map - saliency_map.min())
+              norm_saliency_map /= saliency_map.max() - saliency_map.min()
 
-              # how much increase if keeping the highlighted region
-              # predication on masked input
               output = self.model(input * norm_saliency_map)
               output = F.softmax(output, dim=1)
               score = output[0][predicted_class]
-              score_saliency_map.cuda()
+              score_saliency_map = score_saliency_map.to(self.device)
               score_saliency_map +=  score * saliency_map
 
         score_saliency_map = F.relu(score_saliency_map)
-        score_saliency_map_min, score_saliency_map_max = score_saliency_map.min(), score_saliency_map.max()
+        score_saliency_map_min = score_saliency_map.min()
+        score_saliency_map_max = score_saliency_map.max()
 
         if score_saliency_map_min == score_saliency_map_max:
             return None
 
-        score_saliency_map = (score_saliency_map - score_saliency_map_min).div(score_saliency_map_max - score_saliency_map_min).data
-
+        ds1 = score_saliency_map - score_saliency_map_min
+        ds2 = score_saliency_map_max - score_saliency_map_min
+        score_saliency_map = ds1.div(ds2).data
         return score_saliency_map
-
-    def __call__(self, input, class_idx=None, retain_graph=False):
-        return self.forward(input, class_idx, retain_graph)
 
 
 def sc(model, target_layer, x, class_idx, device):
@@ -103,4 +85,4 @@ def sc(model, target_layer, x, class_idx, device):
 
     """
     score_cam = ScoreCAM(model, target_layer, device)
-    return score_cam(x, class_idx=class_idx)
+    return score_cam.forward(x, class_idx)
